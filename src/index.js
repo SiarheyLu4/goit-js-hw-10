e3b1f21308f95e24da6df26f934d20f5b51afc75
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-06-16 23:10:28 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+
+
+function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/ukraine?fields=name,capital,population,flags,languages`)
+        .then(response => {
+            // console.log(response.json());
+            return response.json();
+        });
+};
+
+
+    // .then(countries => {
+    // console.log(countries);
+    // })
+    // .catch(error => {
+    //     console.log(error);
+    // });
+
+
