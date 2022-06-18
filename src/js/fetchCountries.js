ed483c498c9c08c79cec5eb5972b973d5207f16c
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-06-18 17:09:50 +0300

    tr

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,6 +2,18 @@
 export function fetchCountries(name) {
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
+            if (!response.ok) {
+   throw new Error('Ответ сети был не ok.');
+}
             return response.json();
-        });
+        })
+        .catch((err) => {
+        console.log(err)
+    })
 };
+
+
+
+// if (!response.ok) {
+//    throw new Error('Ответ сети был не ok.');
+// }
