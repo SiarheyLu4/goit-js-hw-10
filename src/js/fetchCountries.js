279bd787606efe7325b2d4e08304d432bb798287
Siarhey Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-06-20 17:40:19 +0300

    404 rep

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,11 +1,18 @@
 
 export function fetchCountries(name) {
     return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+//         .then(response => {
+//             if (!response.ok) {
+//    throw new Error('Ответ сети был не ok.');
+// }
+//             return response.json();
+//         })
         .then(response => {
-            if (!response.ok) {
-   throw new Error('Ответ сети был не ok.');
-}
-            return response.json();
+            if (response.ok) {
+                return response.json()
+            }
+
+            throw new Error(response.statusText)
         })
         .catch((err) => {
         console.log(err)
